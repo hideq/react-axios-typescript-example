17af0b43b22fce030351417ff0a457def0e74093
hideq <hs.show3@gmail.com>
2021-10-16 15:23:20 +0900

    fix a error

--- src/services/tutorial.service.ts
+++ src/services/tutorial.service.ts
@@ -1,4 +1,3 @@
-/* other code
 import http from "../http-common";
 import ITutorialData from "../types/tutorial.type"
 
@@ -33,42 +32,3 @@ class TutorialDataService {
 }
 
 export default new TutorialDataService();
-*/
-
-// mycode
-import http from "../http-common";
-import ITutorialData from "../types/tutorial.type"
-
-class TutorialDataService {
-  getAll() {
-    return http.get("/tutorials");
-  }
-
-  get(id: string) {
-    return http.get(`/tutorials/${id}`);
-  }
-
-  create(data: ITutorialData) {
-    return http.post("/tutorials", data);
-  }
-
-  update(data: ITutorialData, id: any) {
-    return http.put(`/tutorials/${id}`, data);
-  }
-
-  delete(id: any) {
-    return http.delete(`/tutorials/${id}`);
-  }
-
-  deleteAll() {
-    return http.delete(`/tutorials`);
-  }
-
-  findByTitle(title: string) {
-    return http.get(`/tutorials?title=${title}`);
-  }
-}
-
-export default new TutorialDataService();
-
-//*/
